cfdb69e19f201a3247cc54ab3678e3ca4d0608ee
RitwickJoshi <ritwickjoshi@gmail.com>
2020-02-09 15:07:03 +0000

    Did a list comprehension and printed

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hey this is Ritwick Joshi")
+
+names=["ritwick","joshi","john","doe"]
+
+listt = [[name] for name in names] #creating a 2d list
+
+for name in listt:
+  if len(name) == 1:
+    print(name[0])

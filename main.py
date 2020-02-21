299abaa6bb3e0201fe70d5df652819728392afc6
RitwickJoshi <ritwickjoshi@gmail.com>
2020-02-21 04:46:00 +0000

    just did a little bit of clean up of code

--- main.py
+++ main.py
@@ -19,22 +19,23 @@ def merge(even_list, odd_list):
 
     return merged_list
 
-input_string = input()
-input_string_2 = input_string.split(r'[0-9]|\D')
-symbols = re.findall(r'\D',input_string)
-numbers = re.findall(r'\d',input_string)
-count = len(symbols)
-joined_list = []
-final_list_even = []
-final_list_odd = []
-if count%2==0:
-    pass
-else:
-    for number in numbers:
-        if int(number)%2==0:
-            final_list_even.append(number)
-        else:
-            final_list_odd.append(number)
+def challenge_one():
+    input_string = input()
+    symbols = re.findall(r'\D',input_string)
+    numbers = re.findall(r'\d',input_string)
+    count = len(symbols)
+    final_list_even = []
+    final_list_odd = []
+    if count%2==0:
+        pass
+    else:
+        for number in numbers:
+            if int(number)%2==0:
+                final_list_even.append(number)
+            else:
+                final_list_odd.append(number)
 
-final_list = merge(final_list_even, final_list_odd)
-print(perform_join(final_list))
+    final_list = merge(final_list_even, final_list_odd)
+    print(perform_join(final_list))
+
+challenge_one()
